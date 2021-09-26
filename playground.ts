a6f592fdc85e0c379e0dcbe53f5e8075bf6d8924
BMoClay <benmorgancleveland@gmail.com>
2021-09-26 18:34:56 -0400

    array index types walkthrough complete

--- playground.ts
+++ playground.ts
@@ -3,17 +3,36 @@ interface Person {
     name: string
 }
 
-type SingleType<T> = T extends any[] ? T[number] : T
+
+// type CustomArray = { 
+//     [index: number]: string
+// }
+
+// type CustomArray<T = any> = {  // could also be default value like string <T = string>
+//     [index: number]: T
+// }
+
+//could also do the above functionality in the export below by not defining the T default
+// value above, but rather below in the function line 24
+
+type CustomArray<T> = {
+    [index: number]: T
+}
 
 export default function play() {
 
-    type Type1 = SingleType<string[]>
-    type Type2 = SingleType<number[]>
-    type Type3 = SingleType<Person>
+    const items: CustomArray<string> = ["1","2","3"]
+    const items2: CustomArray<number> = [1,2,3]
+    // const items: CustomArray = ["1","2","3"]
 
-    type Type4 = string[][number]
+    //type Single = CustomArray[number]
+    // accessing type CustomArray information from line 7,8
+
+    //if you want to make generic arrays look to line 11,12 <T>
+
 } 
 
 
 
 
+ 
