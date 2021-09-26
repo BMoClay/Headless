8faf6267db3a7fac797a89e77e37fa6aca7b8a71
BMoClay <benmorgancleveland@gmail.com>
2021-09-26 19:22:26 -0400

    infer walkthrough complete

--- playground.ts
+++ playground.ts
@@ -3,27 +3,36 @@ interface Person {
     name: string
 }
 
+// do something to get specific type from a return function
+// right now because T extends () that means it extends an empty function/ takes no parameters
+// type ReturnType<T> = T extends () => string ? string : number
+// this ternary is saying if the the T function comes in w no parameters then 
+// we are assigning a number to the return type
+// otherwise we are assigning the return type to be a string
+
+
+// now we would like to get any function that we pass to T its return type
+// how can i do it
+// instead of writing here a string write Return  -   Will throw error
+// its looking in the ReturnType<T *looking here> for an Return
+// so use infer
+type ReturnType<T> = T extends () => infer Return ? Return : number
+
+
+// remember always replace the types w the generic types
 
 export default function play() {
 
-  function logger(...args: any[]) {
-      return "hello World"
+  function logger() {
+      // now we could change from "hello World" to a different type such as true (boolean)
+      return true     
+      // return "hello World"
   }
-  // implicity logger is a type of string u can check by hovering over
   
-  const kindaLogger: typeof logger = (name: string, age: number) => "Hi Guys"
-
-  const person = {
-      name: "filip",
-      age: 33
-  }
-  // now below the person2 will get the basic structure from person defined above
-  const person2: typeof person = {
-      name: "jon",
-      age: 35
-  }
-
-  kindaLogger("", 20)
+  const loggerReturn: ReturnType<typeof logger> = true
+//   const loggerReturn: ReturnType<typeof logger> = 100
+  // lets break it down. <typeof logger> takes in a parameter thats a string
+  // so when it gets a 2 it doesnt like that? yes
 } 
 
 
