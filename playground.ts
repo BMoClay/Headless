6eadd74e3b90bb645aea3eda7b6821b8a1ae2532
BMoClay <benmorgancleveland@gmail.com>
2021-09-26 19:33:47 -0400

    mulpiple generic parameters walkthrough

--- playground.ts
+++ playground.ts
@@ -5,12 +5,28 @@ interface Person {
     grades: number[]
 }
 
-type PersonKey = keyof Person
+// type Logger = (param1: string, param2: number) => string
+// now w multiple generic types
+
+// type Logger<T, E, R> = (param1: T, param2: E) => R
+// could also be more specific - FP for first parameter, SP for second etc
+type Logger<
+        FP = string,
+        SP = number,
+        RT = string
+        > = (param1: FP, param2: SP) => RT
 
 export default function play() {
 
-    const personKey: PersonKey = "grades"
-  
+    // const superLogger: Logger = (name: string, age: number) => {
+    const superLogger: Logger< string, number, string> = (name, age) => {
+        return "hello Wororld"
+    }
+
+    const logger: Logger = (brand, age) => {
+        return brand + age
+    }
+
 } 
 
 
