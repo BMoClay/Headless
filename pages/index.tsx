11a20e73ec7e8b138fd4c904bd56b1e09f15e436
BMoClay <benmorgancleveland@gmail.com>
2021-09-26 20:53:58 -0400

    dev first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,42 +1,22 @@
 import { useEffect } from "react"
-import play from "../playground"
+
+export async function getStaticProps() {
+  const products = [1,2,3]
+
+  return {
+    props: {
+      products
+    },
+    revalidate: 4 * 60 * 60
+  }
+}
 
 
 export default function Home() {
-
-  useEffect(() =>{
-    play();
-  }, [])  
-
-
+ 
   return (
     <div>
       Hello World!!
     </div>
   )
 }
-
-
-// export default function Home() {
-
-//   //typescript tests and examples: its for declaring types!
-//   const message: string = "Hello World!"
-//   // let age: number = 30
-
-//   let a: AddEventListenerOptions;
-//   // the 'Process' below is from node_modules/next/types/global.d.ts line 4
-//   // it defines if u have a variable w type of process then u can access
-//   // the property with the type of browser
-//   let b: NodeJS.Process
-//   console.log(b.browser)
-
-//   let person: Person = {
-//     name: 'asdf'
-//   }
-
-//   return (
-//     <div>
-//       {message}
-//     </div>
-//   )
-// }
